977bc94c56edce2b87dfbb5448c53762076f29d8
ezequiel <5582340@alu.murciaeduca.es>
2023-06-14 20:33:55 +0200

    añadir los archivos de tournemix que hizo jose endrique a la pagina (Incompleto)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+  import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from 'pages/dashboard';
 import RAdmin from 'components/react-admin/admin';
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,231 +1,114 @@
 import { NavLink } from 'react-router-dom';
-import { useAuth } from 'hooks/auth'
-import reactLogo from 'images/logo512.png';
-// import logo from '../images/recivaper.svg';
-import novedades from '../images/descarga.png';
-import limitada from '../images/images.png';
-import slider1 from '../images/otra-imagen-vaper.png';
-import proximamente from '../images/Imagen Vaper.png';
+import { useAuth } from 'hooks/auth';
 
 function Home() {
-  const { user } = useAuth({ middleware: 'guest' })
+  const { user } = useAuth({ middleware: 'guest' });
 
   return (
-    <div className="relative flex items-top justify-center
-    min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-      <body>
-        <header>
-            <div class="logo-busqueda"><img src='../images/recivaper.svg' class="logo"/>
-                <form><input type="text" placeholder="Buscar producto"/></form>
-            </div>
-            <div class="login">
-                <a href="/dashboard/login">login</a>
-                <label>|</label>
-                <a href="/register">registro</a>
-            </div>
-        </header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="">Vapers</a>
-                </li>
-                <li>
-                    <a href="">Accesorios</a>
-                </li>
-                <li>
-                    <a href="">Novedades</a>
-                </li>
-            </ul>
-        </nav>
-        <div class="registro">
-            <div>
-                <p>¿Interesado en recibir</p>
-                <p>novedades y ofertas?</p>
-                <form><button>REGISTRATE EN RECIVAPER </button></form>
-            </div>
-        </div>
-        <div class="novedades">
-            <div class="imagen-novedades">
-                <img src={novedades} alt=""/>
-            </div>
-            <div class="texto-novedades">
-                <h3>NOVEDADES</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                  Donec rhoncus ullamcorper consequat. Praesent
-                    lobortis varius mauris, sed ultricies ante malesuada vitae. 
-                    In pulvinar ligula sed lacus ornare
-                    lobortis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Donec rhoncus ullamcorper consequat. Praesent
-                    lobortis varius mauris, sed ultricies ante malesuada vitae. 
-                    In pulvinar ligula sed lacus ornare
-                    lobortis.</p>
-            </div>
-        </div>
-        <div class="limitada">
-            <div class="texto-limitada">
-            <h3>EDICIÓN LIMITADA</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Donec rhoncus ullamcorper consequat. Praesent
-                lobortis varius mauris, sed ultricies ante malesuada vitae. 
-                In pulvinar ligula sed lacus ornare lobortis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Donec rhoncus ullamcorper consequat. Praesent
-                lobortis varius mauris, sed ultricies ante malesuada vitae. 
-                In pulvinar ligula sed lacus ornare lobortis.
-            </p>
-        </div>
-            <div class="imagen-limitada">
-                <img src={limitada} alt=""/>
-            </div>
-        </div>
-
-        <section class="ofertas">
-            <h3>OFERTAS</h3>
-
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <div>
-                        <img src={slider1}/>
-                        <p class="precioGrande">Precio 1</p>
-                    </div>
-                    <div>
-                        <img src={slider1}/>
-                        <p class="precioGrande">Precio 2</p>
-                    </div>
-                    <div>
-                        <img src={slider1}/>
-                        <p class="precioGrande">Precio 3</p>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <div>
-                        <img src={slider1}/>
-                        <p class="precioGrande">Precio 1</p>
-                    </div>
-                    <div>
-                        <img src={slider1}/>
-                        <p class="precioGrande">Precio 2</p>
-                    </div>
-                    <div>
-                        <img src={slider1}/>
-                        <p class="precioGrande">Precio 3</p>
-                    </div>
-                  </div>
+    <div className="relative flex items-top justify-center min-h-screen 
+    bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+      <header>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="row">
+                {/* LOGO */}
+                <div className="col-6" id="logo">
+                  <a className="navbar-brand d-none d-md-block" href="#">
+                    <img src="./images/Logo.png" alt="Logo" />
+                  </a>
+                  <h1>TourneyMix</h1>
                 </div>
-                <button class="carousel-control-prev" type="button" 
-                data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" 
-                data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
+                {/* INICIO SESIÓN/REGISTRARSE */}
+                <div className="col-6 d-flex justify-content-end align-items-center" id="inicioRegistro">
+                  <button id="botonInicio" className="btn btn-dark">Iniciar sesión</button>
+                  <button id="botonRegistro" className="btn btn-dark">Registro</button>
+                </div>
               </div>
+            </div>
+          </div>
+          {/* MENÚ DE NAVEGACIÓN */}
+          <div className="row">
+            <div className="col-12 d-flex justify-content-center">
+              <nav className="navbar navbar-expand-md">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Lista de Torneos</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Top de Ganadores</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Acerca de Nosotros</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* Carousel */}
+      <div id="demo" className="carousel slide" data-bs-ride="carousel">
+        {/* Indicators/dots */}
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="0" className="active"></button>
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+        </div>
+        {/* The slideshow/carousel */}
+        <div className="carousel-inner">
+          <div className="carousel-item active" id="carruselVideojuego">
+            <div className="carousel-caption">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus ullamcorper consequat. 
+                Praesent lobortis varius mauris, sed ultricies ante malesuada vitae. Donec rhoncus ullamcorper 
+                consequat. Praesent lobortis varius mauris.</p>
+            </div>
+          </div>
+          <div className="carousel-item" id="carrouselDeporte">
+            <div className="carousel-caption">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus ullamcorper consequat. 
+                Praesent lobortis varius mauris, sed ultricies ante malesuada vitae. Donec rhoncus ullamcorper
+                 consequat. Praesent lobortis varius mauris</p>
+            </div>
+          </div>
+          <div className="carousel-item" id="carrouselJuegoDeMesa">
+            <div className="carousel-caption">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus ullamcorper consequat. 
+                Praesent lobortis varius mauris, sed ultricies ante malesuada vitae. Donec rhoncus ullamcorper 
+                consequat. Praesent lobortis varius mauris</p>
+            </div>
+          </div>
+        </div>
+        {/* Left and right controls/icons */}
+        <button className="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon"></span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+          <span className="carousel-control-next-icon"></span>
+        </button>
+      </div>
+
+      {/* Sección de Crear un torneo */}
+      <div className="container-fluid">
+        <section id="creaUnTorneo">
+          <h2>Empieza creando tu primer torneo</h2>
+          <p></p>
+          <button className="btn btn-info">CREAR TORNEO</button>
+          <p></p>
+          <img src="./images/ImagenPaginaTorneos.png" alt="Torneo" />
         </section>
-
-        <section class="proximamente">
-            <h3>PRÓXIMAMENTE</h3>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 1</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 2</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 3</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 4</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 5</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 6</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 7</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 8</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 9</p>
-                <button>AVÍSAME</button>
-            </article>
-            <article>
-                <img src={proximamente}/>
-                <p>Nombre producto 10</p>
-                <button>AVÍSAME</button>
-            </article>
-        </section>
-
-        <footer>
-            <img src="../images/recivaper_sin_letras.svg" alt="Vaper"/>
-
-            <div class="redes">
-                <a href="https://www.facebook.com">
-                    <img src="img/facebook.svg" alt="Facebook"/>
-                </a>
-                <a href="https://www.instagram.com">
-                    <img src="img/instagram.svg" alt="Instagram"/>
-                </a>
-                <a href="https://www.twitter.com">
-                    <img src="img/twitter.svg" alt="Twitter"/>
-                </a>
-            </div>
-            <div class="menuPie">
-                <ul>
-                    <li>
-                        <a href="/">Vapers</a>
-                    </li>
-                    <li>
-                        <a href="/">Accesorios</a>
-                    </li>
-                    <li>
-                        <a href="/">Novedades</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="telefonoMovilCorreo">
-                <ul>
-                    <li>
-                        <p>Teléfono: 123456789</p>
-                    </li>
-                    <li>
-                        <p>Móvil: 000000000</p>
-                    </li>
-                    <li>
-                        <p>Correo: correo@gmail.com</p>
-                    </li>
-                </ul>
-            </div>
-            <p>Política de cookies · Aviso legal · Política de privacidad · Condiciones de compra</p>
-        </footer>
-
-        <script src="./bootstrap-5.3.0-alpha1/js/src/carousel.js"></script>
-    </body>
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     </div>
   );
 }

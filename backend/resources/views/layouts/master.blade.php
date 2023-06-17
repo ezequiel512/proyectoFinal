de128d16f9e2bb79ed2e9a59283bbf3bc9574932
ezequiel <5582340@alu.murciaeduca.es>
2023-06-17 18:02:29 +0200

    arreglar el header

--- backend/resources/views/layouts/master.blade.php
+++ backend/resources/views/layouts/master.blade.php
@@ -1,32 +1,6 @@
 <!DOCTYPE HTML>
 
 <html>
-<<<<<<< HEAD
-
-<head>
-    <script src="inicio.js"></script>
-    <link type="text/css" rel="stylesheet" href="../../assets/css/inicio.css" />
-    <meta charset="UTF-8">
-    <title>Webest</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="../../assets/Imagen/Favicon.ico">
-</head>
-<header>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="row">
-                    <!-- LOGO -->
-                    <div class="col-6" id="logo">
-                        <a class="navbar-brand d-none d-md-block" href="/   ">
-                            <img src="../../assets/Imagen/Logo.png" alt="Logo" />
-                        </a>
-                    </div>
-                    <!-- INICIO SESIÓN/REGISTRARSE -->
-                    <div class="col-6 d-flex justify-content-end align-items-center" id="inicioRegistro">
-                        <a href="../../login" class="btn btn-dark">Iniciar sesión</a>
-                        <a href="{{ route('register') }}" class="btn btn-dark">Registro</a>
-=======
     <head>
         <script src="inicio.js"></script>
         <link type="text/css" rel="stylesheet" href="../../assets/css/inicio.css" />
@@ -51,7 +25,6 @@
                             <a href="../../login" class="btn btn-dark">Iniciar sesión</a>
                             <a href="../../registro" class="btn btn-dark">Registro</a>
                         </div>
->>>>>>> 2b93dff233e089633f02c4930f64b4033c651f24
                     </div>
                 </div>
             </div>

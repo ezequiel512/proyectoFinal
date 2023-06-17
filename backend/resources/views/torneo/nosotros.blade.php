b5887d74968234f064845ef607434c3ffa3e97d7
ezequiel <5582340@alu.murciaeduca.es>
2023-06-17 13:46:21 +0200

    algunos cambios en el css

--- backend/resources/views/torneo/nosotros.blade.php
+++ backend/resources/views/torneo/nosotros.blade.php
@@ -3,7 +3,6 @@
 
 <head>
     <title>sobre nosotros</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 
 <body>
--- backend/resources/views/torneo/top10.blade.php
+++ backend/resources/views/torneo/top10.blade.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Top 10 Usuarios</title>
 </head>
+
 <body>
     @extends('layouts.master')
     @section('content')
+        <div class="top10">
+            <h1>Top 10 Usuarios con más Torneos Ganados</h1>
+            <ul>
+                @foreach ($usuarios as $posicion => $usuario)
+                    <li class="top10-list">{{ $posicion + 1 }}. {{ $usuario->nombre_usuario }} - {{ $usuario->n_victorias }} victorias</li>
+                @endforeach
 
-    <h1>Top 10 Usuarios con más Torneos Ganados</h1>
-    <ul>
-        @foreach ($usuarios as $usuario)
-            <li>{{ $usuario->nombre_usuario }} - {{ $usuario->n_victorias }} victorias</li>
-        @endforeach
-    </ul>
+            </ul>
+        </div>
     @endsection
 
 </body>
+
 </html>

0ebcacdce41270e9a9f44fc92bfc669dd5be2102
ezequiel <5582340@alu.murciaeduca.es>
2023-06-18 11:31:50 +0200

    arreglado el registro v3

--- backend/app/Models/Usuarios.php
+++ backend/app/Models/Usuarios.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class Usuarios extends Model
+class Usuarios extends Authenticatable implements AuthenticatableContract
 {
     use HasFactory;
+
+    protected $fillable = ['nombre_usuario', 'correo_electronico', 'contrasenya', 'rol'];
 
     protected $primaryKey = 'id_usu';
 
